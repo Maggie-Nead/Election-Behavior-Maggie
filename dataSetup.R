73507aea122f4e9eb9bf814f78be386dd65ca923
Maggie Nead <mnead@wisc.edu>
2020-06-19 23:41:42 -0400

    changed variable names

--- dataSetup.R
+++ dataSetup.R
@@ -15,22 +15,22 @@ members %<>%
 
 
 members %<>%
-  mutate(running105 = ifelse(str_detect(congresses, "105") & str_detect(congresses, "106"), 1, 0))%>%
-  mutate(running106 = ifelse(str_detect(congresses, "106") & str_detect(congresses, "107"), 1, 0))%>%
-  mutate(running107 = ifelse(str_detect(congresses, "107") & str_detect(congresses, "108"), 1, 0))%>%
-  mutate(running108 = ifelse(str_detect(congresses, "108") & str_detect(congresses, "109"), 1, 0))%>%
-  mutate(running109 = ifelse(str_detect(congresses, "109") & str_detect(congresses, "110"), 1, 0))%>%
-  mutate(running110 = ifelse(str_detect(congresses, "110") & str_detect(congresses, "111"), 1, 0))%>%
-  mutate(running111 = ifelse(str_detect(congresses, "111") & str_detect(congresses, "112"), 1, 0))%>%
-  mutate(running112 = ifelse(str_detect(congresses, "112") & str_detect(congresses, "113"), 1, 0))%>%
-  mutate(running113 = ifelse(str_detect(congresses, "113") & str_detect(congresses, "114"), 1, 0))%>%
-  mutate(running114 = ifelse(str_detect(congresses, "114") & str_detect(congresses, "115"), 1, 0))%>%
-  mutate(running115 = ifelse(str_detect(congresses, "115") & str_detect(congresses, "116"), 1, 0))
+  mutate(elected106 = ifelse(str_detect(congresses, "105") & str_detect(congresses, "106"), 1, 0))%>%
+  mutate(elected107 = ifelse(str_detect(congresses, "106") & str_detect(congresses, "107"), 1, 0))%>%
+  mutate(elected108 = ifelse(str_detect(congresses, "107") & str_detect(congresses, "108"), 1, 0))%>%
+  mutate(elected109 = ifelse(str_detect(congresses, "108") & str_detect(congresses, "109"), 1, 0))%>%
+  mutate(elected110 = ifelse(str_detect(congresses, "109") & str_detect(congresses, "110"), 1, 0))%>%
+  mutate(elected111 = ifelse(str_detect(congresses, "110") & str_detect(congresses, "111"), 1, 0))%>%
+  mutate(elected112 = ifelse(str_detect(congresses, "111") & str_detect(congresses, "112"), 1, 0))%>%
+  mutate(elected113 = ifelse(str_detect(congresses, "112") & str_detect(congresses, "113"), 1, 0))%>%
+  mutate(elected114 = ifelse(str_detect(congresses, "113") & str_detect(congresses, "114"), 1, 0))%>%
+  mutate(elected115 = ifelse(str_detect(congresses, "114") & str_detect(congresses, "115"), 1, 0))%>%
+  mutate(elected116 = ifelse(str_detect(congresses, "115") & str_detect(congresses, "116"), 1, 0))
 
 class(members$icpsr)
 
 members %<>%
-  select(running105, running106, running107, running108, running109, running110, running111, running112, running113, running114, running115, icpsr)
+  select(elected106, elected107, elected108, elected109, elected110, elected111, elected112, elected113, elected114, elected115, elected116, icpsr)
 
 data = merge(all_contacts, members, by = "icpsr")
 
