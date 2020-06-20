a1381231be80655dd4017f1b855ae62fe7c448f1
Maggie Nead <mnead@wisc.edu>
2020-06-19 23:53:31 -0400

    Deleted code added to dataSetup and mapped type for each congress

--- Data Descriptives.R
+++ Data Descriptives.R
@@ -1,23 +1,66 @@
-data <- all_contacts
+#Data Descriptives
 
+#Reorder data
 data %<>%
-  mutate(congress = as.character(congress))
-class(data$congress)
+  select(bioname, congress, elected106, elected107, elected108, elected109, elected110, elected111, elected112, elected113, elected114, elected115, elected116, everything())
 
-data %<>%
-  group_by(bioname) %>%
-  mutate(n = n(),
-         SUBJECTS = str_c(SUBJECT, collapse = "; "),
-         IDs = str_c(ID, collapse = "; "),
-         years = str_c(year, collapse = "; "),
-         DATES = str_c(DATE, collapse = "; "),
-         agencies = str_c(agency, collapse = "; "),
-         FROM = str_c(FROM, collapse = "; "),
-         congresses = str_c(as.character(congress), collapse = "; "))  %>%
-  arrange(-n) %>%
-  select(-SUBJECT, -ID, -year, -DATE, -agency, -timeframe, -FROM, -congress, -bioname_year) %>%
-  ungroup() %>%
-  distinct()
+
+#Counts
+
+hist(data$congress)
+
+data %<>% as.numeric(TYPE)
+hist(as.numeric(data$TYPE))
+
+cong106 <- data %>%
+  filter(congress %in% 106)
+hist(as.numeric(cong106$TYPE))
+
+
+cong107 <- data %>%
+  filter(congress %in% 107)
+hist(as.numeric(cong107$TYPE))
+
+cong108 <- data %>%
+  filter(congress %in% 108)
+hist(as.numeric(cong108$TYPE))
+
+cong109 <- data %>%
+  filter(congress %in% 109)
+hist(as.numeric(cong109$TYPE))
+
+cong110 <- data %>%
+  filter(congress %in% 110)
+hist(as.numeric(cong110$TYPE))
+
+cong111 <- data %>%
+  filter(congress %in% 111)
+hist(as.numeric(cong111$TYPE))
+
+cong112 <- data %>%
+  filter(congress %in% 112)
+hist(as.numeric(cong112$TYPE))
+
+cong113 <- data %>%
+  filter(congress %in% 113)
+hist(as.numeric(cong113$TYPE))
+
+cong114 <- data %>%
+  filter(congress %in% 114)
+hist(as.numeric(cong114$TYPE))
+
+cong115 <- data %>%
+  filter(congress %in% 115)
+hist(as.numeric(cong115$TYPE))
+
+cong116 <- data %>%
+  filter(congress %in% 116)
+hist(as.numeric(cong116$TYPE))
+
+data %>%
+  count(ifelse(str_detect()))
+
+
 
 data %<>%
   select(bioname, congresses, everything())
@@ -32,14 +75,6 @@ type %<>%
 
 hist(type$TYPE)
 
-data %<>%
-  group_by(bioname) %>%
-  mutate(n = n(),
-         congresses = str_c(congress, collapse = ";")) %>%
-  arrange(-n) %>%
-  select(-congress) %>%
-  ungroup() %>%
-  distinct()
 
 data %<>%
 select(bioname, congresses, everything())
@@ -55,30 +90,7 @@ hist(type2$TYPE)
 
 class(members$congress)
 
-                                            
-members %<>%
-  mutate(congress = as.character(congress))%>%
-  group_by(bioname) %>%
-  mutate(n = n(),
-         congresses = str_c(congress, collapse = "; ")) %>%
-  arrange(-n) %>%
-  select(-congress) %>%
-  ungroup() %>%
-  distinct()
 
-
-members %<>%
-  mutate(elected1 = ifelse(str_detect(congresses, "105") & str_detect(congresses, "106"), 1, NA))%>%
-  mutate(elected2 = ifelse(str_detect(congresses, "106") & str_detect(congresses, "107"), 1, NA))%>%
-  mutate(elected3 = ifelse(str_detect(congresses, "107") & str_detect(congresses, "108"), 1, NA))%>%
-  mutate(elected4 = ifelse(str_detect(congresses, "108") & str_detect(congresses, "109"), 1, NA))%>%
-  mutate(elected5 = ifelse(str_detect(congresses, "109") & str_detect(congresses, "110"), 1, NA))%>%
-  mutate(elected6 = ifelse(str_detect(congresses, "110") & str_detect(congresses, "111"), 1, NA))%>%
-  mutate(elected7 = ifelse(str_detect(congresses, "111") & str_detect(congresses, "112"), 1, NA))%>%
-  mutate(elected8 = ifelse(str_detect(congresses, "112") & str_detect(congresses, "113"), 1, NA))%>%
-  mutate(elected9 = ifelse(str_detect(congresses, "113") & str_detect(congresses, "114"), 1, NA))%>%
-  mutate(elected10 = ifelse(str_detect(congresses, "114") & str_detect(congresses, "115"), 1, NA))%>%
-  mutate(elected11 = ifelse(str_detect(congresses, "115") & str_detect(congresses, "116"), 1, NA))
                                               
                                               
 
