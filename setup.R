0850bfe812448e1a06e06d9f846a5405a3368fe6
Maggie Nead <mnead@wisc.edu>
2020-06-21 14:39:10 -0400

    working version of setup

--- setup.R
+++ setup.R
@@ -1,5 +1,7 @@
   options(stringsAsFactors = FALSE)
   
+  
+  
   requires <- c("gmailr", "dplyr", "ggplot2", "gdata", "magrittr","googlesheets4","googledrive","devtools","stringi","stringr", "tidyverse",
                 "pdftools", "here", "rvest","maps", "ineq", "mapproj", "dotwhisker", "crayon")
   to_install <- c(requires %in% rownames(installed.packages()) == FALSE)
@@ -54,22 +56,7 @@
   library(crayon)
   library(tidyverse)
   
-  source(here("functions/clean.R")) # data cleaning and intercoder agreement functions 
-  source(here("functions/stateFromLower.R")) # format state names
-  source(here("functions/dateMethods.R"))
-  source(here("functions/nameMethods.R")) # functions for cleaning member names to match the augmented member file
-  
-  #source(here("members/nameCongress.R")) # augments voteview member names
-  ## Load augmented member names without having to load voteview package
-  load(here("members/members.Rdata"))
-  
-  ## Load committee data
-  load(here("data/committees.Rdata"))
-  
-  ## Load typos and date corrections
-  source(here("members/MemberNameTypos.R"))
-  source(here("members/MemberNameDateCorrections.R"))
-
+ 
   
   knitr::opts_chunk$set(echo = TRUE, # echo = TRUE means that your code will show
                         warning = FALSE,
@@ -78,24 +65,7 @@
                         fig.path= 'Figs/', ## where to save figures
                         fig.height = 3,
                         fig.width = 3)
-  
-  data_complete <- function(){
-    if( length(data_list$agency) != length(unique(df$agency)) ){
-      base::message("missing from d:", paste(data_list$agency[!data_list$agency %in% unique(d$agency)], collapse = ", "))
-      
-      base::message("missing from df:", paste(data_list$agency[!data_list$agency %in% unique(df$agency)], collapse = ", "))
-      
-      base::message(paste("merge.R stopped at", stopped))
-    }
-    
-    if(length(data_list$agency) == length(unique(df$agency))){
-      base::message(green("Merge complete"))
-    }
-  }
+
 
   
   
-## Since clean scripts were written for the googlesheets packaged, which has been replaced by googlesheets4
-  gs_read <-   function(sheet){sheet %>% googlesheets4::read_sheet(col_types = "c")}
-  
-  gs_title <- function(file.name){googledrive::drive_get(file.name)}
